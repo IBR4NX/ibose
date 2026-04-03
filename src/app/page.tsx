a8dcd903789d325ded0f07821d2f4014c69c1ee6
ibr4nx <ibr4nx.gmail.com>
2026-04-03 21:49:46 +0300

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 import InteractiveHero from '@/ibovs/interactive-hero';
 import wait from '@test/wait'
+import Test from "@/components/styles/test"
 export default async function Home() {
   await wait(60)
 	return (
 		<>
+		<div className="fixed z-40 bg-amber-100">
+		  
+		<Test/>
+		</div>
 		<InteractiveHero />
 		</>
 		);
