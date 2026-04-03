0b0c2989b75e1d0894a57781d538703149c5355e
ibr4nx <ibr4nx.gmail.com>
2026-04-03 22:58:54 +0300

    update

--- src/components/styles/test.tsx
+++ src/components/styles/test.tsx
@@ -3,10 +3,12 @@ import "./loading.css";
 export const Test = () => {
 	return (
 		<div className="container-spin">
-			<svg className="spinner" width="65px" height="65px" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">
+			<svg className="spinner" width="65px" height="65px" viewBox="0 0 66 66" >
 				<circle className="path" fill="none" strokeWidth="6"
-				//strokeLinecap="round"
-				cx="33" cy="33" r="30"></circle>
+				strokeLinecap="round"
+				cx="33" cy="33" r="30"
+				strokeDashoffset={0}
+				/>
 			</svg>
 		</div>
 	);
